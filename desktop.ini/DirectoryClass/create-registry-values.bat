828023688d44702938f91850bf5c549dc80a9a5f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-07 15:15:49 +0100

    n/a

--- desktop.ini/DirectoryClass/create-registry-values.bat
+++ desktop.ini/DirectoryClass/create-registry-values.bat
@@ -13,3 +13,6 @@ reg add HKCR\tq84-directory-desktop-ini-test\shell\TQ84Verb\command
 reg add HKCR\tq84-directory-desktop-ini-test\shell\TQ84Verb\command /ve /d "%systemroot%\system32\notepad.exe %1\desktop.ini" /f
 
 attrib +s +h Desktop.ini
+
+rem ???
+attrib +s . 
