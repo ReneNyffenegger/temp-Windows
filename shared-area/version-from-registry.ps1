ab7931ee3cd47cd9f379ffe2df615d308dd775be
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-03-05 19:08:48 +0100

    Update version-from-registry.ps1

--- shared-area/version-from-registry.ps1
+++ shared-area/version-from-registry.ps1
@@ -4,3 +4,9 @@ get-itemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' | select-o
   curretnMinorVersionNumber,
   releaseId,
   productName
+
+CurrentBuild              : 17763
+CurrentMajorVersionNumber : 10
+curretnMinorVersionNumber :
+ReleaseId                 : 1809
+ProductName               : Windows 10 Pro
