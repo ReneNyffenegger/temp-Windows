e72012ec1582f267fe0b0a014ba542c58141a132
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-02-21 17:31:36 +0100

    .

--- GetSleepAndHibernateValues.ps1
+++ GetSleepAndHibernateValues.ps1
@@ -12,3 +12,29 @@ $hibernate = powercfg -query $planGuid SUB_SLEEP HIBERNATEIDLE
 $hibernateValue = $hibernate| Select-String "Current AC Power Setting Index"
 Write-Host "Sleep After:" $sleepValue.ToString().Split(":")[1]
 Write-Host "Hibernate After:" $hibernateValue.ToString().Split(":")[1]
+
+
+# A non working attempt by myself (tq84);
+
+
+$plan = Get-cimInstance -Class win32_powerplan -Namespace root\cimv2\power  -Filter "isActive='true'" 
+$regex = [regex]"{(.*?)}$"
+$planGuid = $regex.Match($plan.instanceID.Tostring()).groups[1].value
+# powercfg -query $planGuid
+
+
+# $pwrSet = Get-cimInstance -Namespace root\cimv2\power  -Class win32_powerSetting  -filter "instanceId='Microsoft:PowerSetting\\{29f6c1db-86da-48c5-9fdb-f2b67b1f44da}'"
+
+# $config = $pwrSet | where-object {$_.instanceId -eq 'Microsoft:PowerSetting\{29f6c1db-86da-48c5-9fdb-f2b67b1f44da}'}
+
+$config = Get-cimInstance -Namespace root\cimv2\power  -Class win32_powerSetting  -filter "instanceId='Microsoft:PowerSetting\\{29f6c1db-86da-48c5-9fdb-f2b67b1f44da}'"
+
+
+
+<#
+Caption        :
+Description    : Specify how long your computer is inactive before going to sleep.
+ElementName    : Sleep after
+InstanceID     : Microsoft:PowerSetting\{29f6c1db-86da-48c5-9fdb-f2b67b1f44da}
+PSComputerName :
+#>
